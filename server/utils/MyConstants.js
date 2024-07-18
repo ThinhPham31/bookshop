35fe336ede8bc991c7b22e0564ed8d2f299478e4
ThinhPham31 <143172185+ThinhPham31@users.noreply.github.com>
2024-07-18 17:00:09 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.dca36qq.mongodb.net',
+  DB_USER: 'thinh',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline2',
+  JWT_SECRET: 'KHOA_BI_MAT',
+  JWT_EXPIRES: '888895628972654872', // in milliseconds
+  EMAIL_USER: 'thinhpham.25052016@gmail.com', // gmail service
+  EMAIL_PASS: 'ktlawnydwonhcpbk'
+};
+module.exports = MyConstants;
